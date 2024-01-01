a358dc0084c47b56cdf44690889aacc62d4914f5
gefe <georgios@gdis.se>
2024-01-01 20:47:32 +0100

    Add files via upload

--- web/app.py
+++ web/app.py
@@ -1,1 +1,54 @@
+from flask import Flask, render_template, request
+import subprocess
+import os
+import time
 
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    wifi_ap_array = scan_wifi_networks()
+    return render_template('app.html', wifi_ap_array = wifi_ap_array)
+
+@app.route('/save_credentials', methods = ['GET', 'POST'])
+def save_credentials():
+    ssid = request.form['ssid']
+    wifi_key = request.form['wifi_key']
+    create_login(ssid, wifi_key)
+    os.system('mv wifi.tmp /root/wifi')
+    os.system('systemctl stop net')
+    os.system('nmcli device disconnect wlan0')
+    os.system('bash /root/wifi')
+    time.sleep(1)
+    os.system('systemctl start net')
+    time.sleep(1)
+    os.system('reboot')
+
+######## FUNCTIONS ##########
+
+def scan_wifi_networks():
+    iwlist_raw = subprocess.Popen(['iwlist', 'scan'], stdout=subprocess.PIPE)
+    ap_list, err = iwlist_raw.communicate()
+    ap_array = []
+
+    for line in ap_list.decode('utf-8').rsplit('\n'):
+        if 'ESSID' in line:
+            ap_ssid = line[27:-1]
+            if ap_ssid != '':
+                ap_array.append(ap_ssid)
+
+    return ap_array
+
+def create_login(ssid, wifi_key):
+
+    temp_conf_file = open('wifi.tmp', 'w')
+
+    temp_conf_file.write('#!/bin/bash\n')
+    temp_conf_file.write('\n')
+    temp_conf_file.write('nmcli dev wifi connect ' + ssid + '  password  ' + wifi_key + '\n')
+    temp_conf_file.close
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 80)
